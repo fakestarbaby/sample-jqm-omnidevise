5c7cf2816be012cb54ecd5db98bdf49782d70fa7
fakestarbaby <fakestarbaby@gmail.com>
2011-08-08 18:12:40 +0900

    初回コミット。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleJqmOmnidevise::Application.config.secret_token = '43c8b4891748e4cdc377c956c493b47b6b55cbd19b3dfdef8401e0ed60d74605c65ddd2744022effa58957a9bc7a771084fec53215ee167a31bb0ab1da42c26e'
